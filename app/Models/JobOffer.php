505de98c5c83390810a747d238fe1da0d6f68044
kobe <youcode@skiff.com>
2024-02-20 16:20:37 +0100

    add my models

--- app/Models/JobOffer.php
+++ app/Models/JobOffer.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class JobOffer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'salary',
+        'deadline',
+    ];
+
+
+    protected $casts = [
+        'deadline' => 'date',
+    ];
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
+    }
 }
