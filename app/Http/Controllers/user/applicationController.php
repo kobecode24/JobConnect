9b071a5ec819c14132c3b626c7edd1003cdb8e9f
Echaftech23 <rachidchaf2001@gmail.com>
2024-02-27 12:00:00 +0100

    added user application.

---
+++ app/Http/Controllers/user/applicationController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\user;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreApplicationRequest;
+use App\Http\Requests\UpdateApplicationRequest;
+use App\Models\Application;
+use App\Models\Skill;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class ApplicationController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+        $applications = Application::latest()->get();
+
+        return view('ceo.applications.index', compact('applications'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreApplicationRequest $request)
+    {
+
+        $hr = User::create($request->validated());
+
+        $hr->roles()->attach($request->role_id);
+        $hr->skills()->sync($request->input($request->skill_id, []));
+
+        return redirect()->route('applications')->with('success', 'Application added successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request)
+    {
+        $application = User::findOrFail($request);
+
+
+        return view('ceo.applications.show', compact('application', 'skills'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Request $request)
+    {
+        $hr = User::findOrFail($request);
+
+        $skills = Skill::latest()->get();
+
+        return view('ceo.applications.edit', compact('application', 'skills'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateApplicationRequest $request)
+    {
+        $hr = User::findOrFail($request);
+
+        $hr->update($request->validated());
+        $hr->skills()->sync($request->skill_id);
+
+        return redirect()->route('applications')->with('success', 'HR updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request)
+    {
+        $hr = User::findOrFail($request);
+
+        $hr->delete();
+
+        return redirect()->route('applications')->with('success', 'Application deleted successfully');
+    }
+}
