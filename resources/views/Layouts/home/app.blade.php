e5967a98a8116a4a669e4ceeee3e362115f1c337
Echaftech23 <rachidchaf2001@gmail.com>
2024-02-27 12:01:48 +0100

    update Layout app.

--- resources/views/Layouts/home/app.blade.php
+++ resources/views/Layouts/home/app.blade.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JobConnect Plateform</title>
+    <link rel="stylesheet" href="{{ mix('resources/css/app.css') }}" />
     <link rel="stylesheet" href="{{ mix('resources/css/style.css') }}" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
 </head>
 <body>
 
